4a3961f84f50631f18426ab8ab3acbaedb953602
mattiaachilli <mattia.achilli@studio.unibo.it>
2021-11-05 20:41:37 +0100

    Add snippet about Memoization

---
+++ src/main/scala/Snippets/Concurrent/Memoization.scala
@@ -0,0 +1,16 @@
+package Snippets.Concurrent
+
+import cats.effect.{ExitCode, IO, IOApp}
+
+object Memoization extends IOApp {
+
+  override def run(args: List[String]): IO[ExitCode] = {
+    val action: IO[String] = IO.println("This is only printed once").as("action")
+    for {
+        memoized <- action.memoize
+        res1 <- memoized
+        res2 <- memoized
+        _ <- IO.println(res1 ++ res2)
+    } yield ExitCode.Success
+  }
+}
