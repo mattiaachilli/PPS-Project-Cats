84220dc77c7b4948f1c226b82c85852eb608c514
mattiaachilli <mattia.achilli@studio.unibo.it>
2021-11-05 20:41:19 +0100

    Add snippet about Temporal

---
+++ src/main/scala/Snippets/Temporal/SleepExample.scala
@@ -0,0 +1,13 @@
+package Snippets.Temporal
+
+import cats.effect.{ExitCode, IO, IOApp, Sync}
+
+import scala.concurrent.duration.DurationInt
+
+object SleepExample extends IOApp {
+  override def run(args: List[String]): IO[ExitCode] = {
+    for {
+      _ <- IO.println("Hello World 1") >> Sync[IO].sleep(5.seconds) >> IO.println("Hello World 2")
+    } yield ExitCode.Success
+  }
+}
