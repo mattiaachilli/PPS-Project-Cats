daa0cabd540dbb302e61a189b3a175570e24230c
mattiaachilli <mattia.achilli@studio.unibo.it>
2021-11-06 20:29:59 +0100

    Add snippet about CountDownLatch and CyclicBarrier

---
+++ src/main/scala/Snippets/CountDownLatch/CountDownLatchExample.scala
@@ -0,0 +1,16 @@
+package Snippets.CountDownLatch
+
+import cats.effect.std.CountDownLatch
+import cats.effect.{IO, IOApp}
+
+object CountDownLatchExample extends IOApp.Simple {
+  override def run: IO[Unit] = {
+    for {
+      latch <- CountDownLatch[IO](2)
+      fiber <- (latch.await >> IO.println("Latch unlocked")).start
+      _ <- latch.release >> IO.println("First release")
+      _ <- latch.release >> IO.println("Second release")
+      _ <- fiber.join
+    } yield()
+  }
+}
---
+++ src/main/scala/Snippets/CyclicBarrier/CyclicBarrierExample.scala
@@ -0,0 +1,19 @@
+package Snippets.CyclicBarrier
+
+import cats.effect.std.CyclicBarrier
+import cats.effect.{IO, IOApp}
+import cats.implicits._
+
+import scala.concurrent.duration.DurationInt
+
+object CyclicBarrierExample extends IOApp.Simple {
+  override def run: IO[Unit] = {
+    for {
+      barrier <- CyclicBarrier[IO](2)
+      fiberA <- (IO.println("First fiber") >> barrier.await >> IO.println("First fiber after barrier")).start
+      fiberB <- (IO.println("Second fiber") >> barrier.await >> IO.sleep(1.second) >>
+        IO.println("Second fiber after barrier")).start
+      _ <- (fiberA.join, fiberB.join).tupled
+    } yield ()
+  }
+}
