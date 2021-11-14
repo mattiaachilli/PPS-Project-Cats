b3f1863f5f872aad398636971ea1b98280243a10
mattiaachilli <mattia.achilli@studio.unibo.it>
2021-11-14 17:28:16 +0100

    wip

--- src/main/scala/rest/api/MoviesRepository.scala
+++ src/main/scala/rest/api/MoviesRepository.scala
@@ -1,72 +1,190 @@
 package rest.api
 
-import cats.effect.Sync
+import cats.effect.{Async, IO, Sync}
+import cats.implicits._
+import io.circe.Json
+import org.http4s.EntityDecoder
+import org.http4s.circe.jsonOf
+import org.http4s.client.Client
 import rest.api.Entities._
+import rest.api.Routes.IMDB
+import io.circe.optics.JsonPath._
 
 import java.util.UUID
+import scala.collection.mutable.ListBuffer
 
-/*
-  Case class where are stored data
- */
-case class MoviesRepository[F[_]: Sync](private val movies: Map[String, Movie],
-                                  private val directors: List[Director]) {
+class MoviesRepository[F[_] : Async](var movies: ListBuffer[MovieWithId], var directors: ListBuffer[Director],
+                                    var actors: ListBuffer[Actor]) {
   private val makeId: F[String] = Sync[F].delay(UUID.randomUUID().toString)
 
-  def getAllMovies: F[List[Movie]] = {
-    Sync[F].delay(movies.values.toList)
-  }
+  /* Movie */
+  def getAllMovies: F[ListBuffer[MovieWithId]] = Sync[F].delay(movies)
 
-  def findMovieById(id: UUID): F[Option[Movie]] = {
-    Sync[F].delay(movies.get(id.toString))
-  }
+  def findMovieById(id: UUID): F[Option[MovieWithId]] = Sync[F].delay(movies.find(_.id == id.toString))
 
-  def findMoviesByDirector(director: String): F[List[Movie]] = director.split(" ") match {
+  def findMoviesByDirectorName(director: String): F[ListBuffer[MovieWithId]] = director.split(" ") match {
     case arr: Array[String] if arr.length == 2 =>
-      Sync[F].delay(movies.values.filter {
-        case movie if movie.director.firstName == arr(0) && movie.director.lastName == arr(1) => true
-        case _ => false
-      }.toList)
-    case _ => Sync[F].delay(List())
+      Sync[F].delay(movies.filter(movieWithId => movieWithId.movie.director.firstName == arr(0)
+        && movieWithId.movie.director.lastName == arr(1)))
+    case _ => Sync[F].delay(ListBuffer())
   }
 
-  def findMoviesByYear(year: Int): F[List[Movie]] = year match {
-    case year if movies.filter()
+  def addMovie(movie: Movie): F[String] = {
+    for {
+      uuid <- makeId
+      _ <- Sync[F].delay {
+        movies += MovieWithId(uuid, movie)
+      }
+    } yield uuid
+  }
+
+  def updateMovie(id: UUID, movie: Movie): F[Unit] = {
+    val index = movies.indexWhere(movie => movie.id == id.toString)
+    movies -= movies(index)
+    movies += MovieWithId(id.toString, movie)
+    Sync[F].unit
+  }
+
+  def deleteMovie(id: UUID): F[Unit] = Sync[F].delay {
+    val index = movies.indexWhere(movie => movie.id == id.toString)
+    movies.remove(index)
+  }
+
+  def findMoviesByYear(year: Int): F[ListBuffer[MovieWithId]] = Sync[F].delay(movies.filter(_.movie.year == year))
+
+  def getRatingByMovie(title: String, client: Client[F])(implicit jsonDecoder: EntityDecoder[F, Json] = jsonOf[F, Json])
+    : F[Unit] = {
+    val informationMovieUrl = IMDB.getRatingUrl(title)
+    val id = for {
+      json <- client.expect[Json](informationMovieUrl)
+      // root.results.index(0).id.string
+    } yield 1
+    Sync[F].unit
+  }
+
+
+  /* Directors */
+  def getAllDirectors: F[ListBuffer[Director]] = Sync[F].delay(directors)
+
+  def findDirectorByName(director: String): F[Boolean] = Sync[F].delay(director.split(" ") match {
+    case arr: Array[String] if arr.nonEmpty => directors.find(d => d.firstName == arr(0) && d.lastName ==
+        arr(1)) match {
+        case Some(_) => true
+        case _ => false
+      }
+      case _ => false
+    }
+  )
+
+  def addDirector(director: Director): F[Boolean] = Sync[F].delay(directors.find(d => d.firstName == director.firstName
+    && d.lastName == director.lastName) match {
+    case Some(_) => false
+    case _ =>
+      directors += director
+      true
+    })
+
+  def updateDirector(oldDirector: String, newDirector: Director): F[Unit] = {
+    val split = oldDirector.split(" ")
+    val index = directors.indexWhere(d => d.firstName == split(0) && d.lastName == split(1))
+    directors -= directors(index)
+    directors += newDirector
+    Sync[F].unit
+  }
+
+  def deleteDirector(director: String): F[Unit] = Sync[F].delay {
+    val split = director.split(" ")
+    val index = directors.indexWhere(d => d.firstName == split(0) && d.lastName == split(1))
+    directors.remove(index)
+  }
+
+  /* Actors */
+  def getAllActors: F[ListBuffer[Actor]] = Sync[F].delay(actors)
+
+  def findActorByName(actor: String): F[Boolean] = Sync[F].delay(actor.split(" ") match {
+    case arr: Array[String] if arr.nonEmpty => actors.find(a => a.firstName == arr(0) && a.lastName ==
+        arr(1)) match {
+        case Some(_) => true
+        case _ => false
+      }
+      case _ => false
+    }
+  )
+
+  def addActor(actor: Actor): F[Boolean] = Sync[F].delay(actors.find(a => a.firstName == actor.firstName
+    && a.lastName == actor.lastName) match {
+    case Some(_) => false
+    case _ =>
+      actors += actor
+      true
+  })
+
+  def updateActor(oldActor: String, newActor: Actor): F[Unit] = {
+    val split = oldActor.split(" ")
+    val index = actors.indexWhere(a => a.firstName == split(0) && a.lastName == split(1))
+    actors -= actors(index)
+    actors += newActor
+    Sync[F].unit
+  }
+
+  def deleteActor(actor: String): F[Unit] = Sync[F].delay {
+    val split = actor.split(" ")
+    val index = actors.indexWhere(a => a.firstName == split(0) && a.lastName == split(1))
+    actors.remove(index)
   }
 }
 
 private object SeedData {
-  val jamesCameron: Director = Director("James", "Cameron", "Canadian", 44)
-  val tonyScott: Director =  Director("Tony", "Scott", "British", 55)
+  private object Directors {
+    val jamesCameron: Director = Director("James", "Cameron", "Canadian", 44)
+    val tonyScott: Director = Director("Tony", "Scott", "British", 55)
+  }
+
+  private object Actors {
+    val actorsTitanic: ListBuffer[Actor] = ListBuffer(Actor("Kate", "Winslet", 57), Actor("Leonardo", "DiCaprio", 44),
+      Actor("Billy", "Zane", 63))
+    val actorsTopGun: ListBuffer[Actor] = ListBuffer(Actor("Tom", "Cruise", 73), Actor("Kelly", "McGillis", 10),
+      Actor("Val", "Kilmer", 25))
+  }
+
+  import Actors._
+  import Directors._
+
   val titanic: Movie = Movie(
-    "9127c44c-7c72-44a8-8bcb-088e3b659eca",
     "Titanic",
     1997,
-    List(Actor("Kate", "Winslet", 57), Actor("Leonardo", "DiCaprio", 44), Actor("Billy", "Zane", 63)),
+    actorsTitanic,
     jamesCameron,
     List("Romance", "Drama", "Epic", "Disaster"),
     2_195_170_204L,
     11
   )
   val topGun: Movie = Movie(
-    "af9ce051-8541-42a9-88c4-e36d5036ad1e",
     "Top Gun",
     1986,
-    List(Actor("Tom", "Cruise", 73), Actor("Kelly", "McGillis", 10), Actor("Val", "Kilmer", 25)),
+    actorsTopGun,
     tonyScott,
     List("Action", "Romance", "Drama", "Adventure"),
     356_800_000L,
     0
   )
-  def movies: Map[String, Movie] = Map(titanic.id -> titanic, topGun.id -> topGun)
-  def directors: List[Director] = List(jamesCameron, tonyScott)
+
+  def movies: ListBuffer[MovieWithId] = ListBuffer(MovieWithId("9127c44c-7c72-44a8-8bcb-088e3b659eca", titanic),
+    MovieWithId("af9ce051-8541-42a9-88c4-e36d5036ad1e", topGun))
+
+  def directors: ListBuffer[Director] = ListBuffer(jamesCameron, tonyScott)
+
+  def actors: ListBuffer[Actor] = actorsTitanic ++ actorsTopGun
 }
 
 object MoviesRepository {
-  def empty[F[_]: Sync]: MoviesRepository[F] = new MoviesRepository[F](Map(), List())
-  def createWithSeedData[F[_]: Sync]: MoviesRepository[F] = {
-    val movies: Map[String, Movie] = SeedData.movies
-    val directorDetails: List[Director] = SeedData.directors
+  def empty[F[_]: Async]: MoviesRepository[F] = new MoviesRepository[F](ListBuffer(), ListBuffer(), ListBuffer())
 
-    new MoviesRepository[F](movies, directorDetails)
+  def createWithSeedData[F[_]: Async]: MoviesRepository[F] = {
+    val movies: ListBuffer[MovieWithId] = SeedData.movies
+    val directors: ListBuffer[Director] = SeedData.directors
+    val actors: ListBuffer[Actor] = SeedData.actors
+
+    new MoviesRepository[F](movies, directors, actors)
   }
 }
--- src/main/scala/rest/api/Routes.scala
+++ src/main/scala/rest/api/Routes.scala
@@ -1,50 +1,112 @@
 package rest.api
 
-import cats.Monad
-import cats.effect.Sync
+import cats.effect.Async
+import cats.effect.implicits._
 import cats.implicits._
 import io.circe.generic.auto._
 import io.circe.syntax._
 import org.http4s._
 import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
 import org.http4s.circe._
+import org.http4s.client.{Client, JavaNetClientBuilder}
+import org.http4s.client.blaze.BlazeClientBuilder
 import org.http4s.dsl.Http4sDsl
-import org.http4s.dsl.impl.{QueryParamDecoderMatcher, ValidatingQueryParamDecoderMatcher}
-import org.http4s.implicits._
+import org.http4s.dsl.impl.{OptionalValidatingQueryParamDecoderMatcher, QueryParamDecoderMatcher, ValidatingQueryParamDecoderMatcher}
+import org.http4s.implicits.http4sKleisliResponseSyntaxOptionT
+import rest.api.Entities.{Actor, Director, Movie, MovieWithId}
 
 import java.time.Year
+import scala.collection.mutable.ListBuffer
+import scala.concurrent.ExecutionContext.global
 
 object Routes {
   /* Endpoints:
-    - GET all movies => Root/movies
-    - GET movie from id => Root/movies/id
-    - GET all movies for a director
-    - GET details about a director
-    - POST a new director
-    - POST a new film
+    - Movies:
+      - GET all movies => Root/movies
+      - GET movie from id => Root/movies/id
+      - GET all movies from a director and year (optional)
+      - GET all movies from a year
+      - ADD a new movie
+      - UPDATE a movie
+      - DELETE a movie
+
+    - Directors:
+      - GET all directors
+      - GET the best director
+      - ADD a new director
+      - UPDATE a director
+      - DELETE a director
+
+    - Actors:
+      - GET all actors
+      - GET the best actor
+      - ADD an actor
+      - UPDATE an actor
+      - DELETE an actor
    */
-  private implicit val yearQueryParamDecoder: QueryParamDecoder[Year] = QueryParamDecoder[Int].map(year => Year.of(year))
+  private object Params {
+    implicit val yearQueryParamDecoder: QueryParamDecoder[Year] = QueryParamDecoder[Int].map(year => Year.of(year))
 
-  private object YearQueryParamMatcher extends ValidatingQueryParamDecoderMatcher[Year]("year")
-  private object DirectorQueryParamMatcher extends QueryParamDecoderMatcher[String]("director")
+    case object YearQueryParamMatcher extends ValidatingQueryParamDecoderMatcher[Year]("year")
 
-  private def movieRoutes[F[_]: Monad: Sync](moviesRepository: MoviesRepository[F]): HttpRoutes[F] = {
+    case object OptionalYearQueryParamMatcher extends OptionalValidatingQueryParamDecoderMatcher[Year]("year")
+
+    case object DirectorQueryParamMatcher extends QueryParamDecoderMatcher[String]("director")
+  }
+
+  object IMDB {
+    private val apiKey = "k_44ltof3p"
+
+    def getInformationMovieUrl(title: String): String = s"https://imdb-api.com/en/API/Search/$apiKey/$title"
+
+    def getRatingUrl(movieId: String): String = s"https://imdb-api.com/en/API/Ratings/$apiKey/$movieId"
+  }
+
+  import Params._
+
+  private def movieRoutes[F[_] : Async](moviesRepository: MoviesRepository[F]): HttpRoutes[F] = {
     val dsl = Http4sDsl[F]
     import dsl._
 
+    val client: Client[F] = JavaNetClientBuilder[F].create
+
     HttpRoutes.of[F] {
       // Get movies by director
-      case GET -> Root / "movies" :? DirectorQueryParamMatcher(director) =>
-        moviesRepository.findMoviesByDirector(director).flatMap {
-          case movies if movies.nonEmpty => Ok(movies.asJson)
-          case _ => NotFound(s"No movie found with director $director")
+      case GET -> Root / "movies" :? DirectorQueryParamMatcher(director) +& OptionalYearQueryParamMatcher(maybeYear) =>
+        val moviesByDirector: F[ListBuffer[MovieWithId]] = moviesRepository.findMoviesByDirectorName(director)
+        maybeYear match {
+          case Some(validatedYear) =>
+            validatedYear.fold(
+              _ => BadRequest("Unable to parse argument year"),
+              year => {
+                moviesByDirector.flatMap {
+                  case movies if movies.nonEmpty => Ok(movies.filter(_.movie.year == year.getValue).asJson)
+                  case _ => NotFound(s"No movies with $director found")
+                }
+              }
+            )
+          case None => moviesByDirector.flatMap {
+            case movies if movies.nonEmpty => Ok(movies.asJson)
+            case _ => NotFound(s"No movies with $director found")
+          }
         }
       // Get movies by year
+      case GET -> Root / "movies" :? YearQueryParamMatcher(yearValidated) =>
+        yearValidated.fold(
+          _ => BadRequest("Unable to parse argument year"),
+          year => {
+            val moviesByYear = moviesRepository.findMoviesByYear(year.getValue)
+            moviesByYear.flatMap {
+              case movies if movies.nonEmpty => Ok(movies.asJson)
+              case _ => NotFound(s"No movies with ${year.getValue} found")
+            }
+          }
+        )
       // Get all movies
       case GET -> Root / "movies" =>
         moviesRepository.getAllMovies.flatMap {
           case movies if movies.nonEmpty => Ok(movies.asJson)
-          case _ => Sync[F].delay(Response(status = NoContent))
+          case _ => NoContent()
         }
       // Get movie by id
       case GET -> Root / "movies" / UUIDVar(movieId) =>
@@ -52,25 +114,123 @@ object Routes {
           case Some(movie) => Ok(movie.asJson)
           case _ => NotFound(s"No movie with $movieId found")
         }
+      // Add a movie
+      case req@POST -> Root / "movies" =>
+        req.decodeJson[Movie]
+          .flatMap(moviesRepository.addMovie)
+          .flatMap(movie => Created(s"Created movie with id: $movie"))
+      // Update a movie
+      case req@PUT -> Root / "movies" / UUIDVar(movieId) =>
+        val exist = moviesRepository.findMovieById(movieId)
+        exist.flatMap {
+          case Some(_) =>
+            req.decodeJson[Movie]
+              .flatMap(movie => moviesRepository.updateMovie(movieId, movie))
+              .flatMap(_ => Ok("Updated successfully"))
+          case _ => NotFound(s"No movie with $movieId found")
+        }
+      // Delete a movie
+      case DELETE -> Root / "movies" / UUIDVar(movieId) =>
+        val exist = moviesRepository.findMovieById(movieId)
+        exist.flatMap {
+          case Some(_) => moviesRepository.deleteMovie(movieId).flatMap(_ => Ok("Deleted successfully"))
+          case _ => NotFound(s"No movie with $movieId found")
+        }
+      // Get the best movie by rating
+      case GET -> Root / "movies" / "ratings" =>
+        moviesRepository.getAllMovies
+          .flatMap(movies => movies
+            .map(_.movie.title)
+            .toVector
+            .parTraverse
+            (title => moviesRepository.getRatingByMovie(title, client))
+          ).flatMap(_ => Ok("Prova"))
     }
   }
 
-//  def directorRoutes[F[_] : Monad](moviesRepository: MoviesRepository[F]): HttpRoutes[F] = {
-//    val dsl = Http4sDsl[F]
-//    import dsl._
-//
-//    HttpRoutes.of[F] {
-//      case GET -> Root / "directors" / DirectorPath(director) =>
-//       directorDetails.get(director) match {
-//          case Some(directorDetails) => Ok(directorDetails.asJson)
-//          case None => NotFound(s"No directory found for: $director")
-//        }
-//    }
-//  }
+  def directorRoutes[F[_] : Async](moviesRepository: MoviesRepository[F]): HttpRoutes[F] = {
+    val dsl = Http4sDsl[F]
+    import dsl._
 
-  private def allRoutes[F[_] : Monad: Sync](moviesRepository: MoviesRepository[F]): HttpRoutes[F] =
-    movieRoutes[F](moviesRepository) // <+> directorRoutes[F](moviesRepository) //Combine of semigroup
+    HttpRoutes.of[F] {
+      // Get all directors
+      case GET -> Root / "directors" =>
+        moviesRepository.getAllDirectors.flatMap {
+          case directors if directors.nonEmpty => Ok(directors.asJson)
+          case _ => NoContent()
+        }
+      // Add a new director
+      case req@POST -> Root / "directors" =>
+        req.decodeJson[Director]
+          .flatMap(moviesRepository.addDirector)
+          .flatMap {
+            case true => Created("Created!")
+            case false => NotAcceptable("This director already exists!")
+          }
+      // Update a director
+      case req@PUT -> Root / "directors" / director =>
+        val exist = moviesRepository.findDirectorByName(director)
+        exist.flatMap {
+          case true =>
+            req.decodeJson[Director]
+              .flatMap(newDirector => moviesRepository.updateDirector(director, newDirector))
+              .flatMap(_ => Ok("Updated successfully"))
+          case _ => NotFound(s"No director found for: $director")
+        }
+      // Delete a director
+      case DELETE -> Root / "directors" / director =>
+        val exist = moviesRepository.findDirectorByName(director)
+        exist.flatMap {
+          case true => moviesRepository.deleteDirector(director).flatMap(_ => Ok("Deleted successfully"))
+          case _ => NotFound(s"No director found for: $director")
+        }
+    }
+  }
 
-  def allRoutesComplete[F[_] : Monad: Sync](moviesRepository: MoviesRepository[F]): HttpApp[F] =
+  def actorRoutes[F[_] : Async](moviesRepository: MoviesRepository[F]): HttpRoutes[F] = {
+    val dsl = Http4sDsl[F]
+    import dsl._
+
+    HttpRoutes.of[F] {
+      // Get all actors
+      case GET -> Root / "actors" =>
+        moviesRepository.getAllActors.flatMap {
+          case actors if actors.nonEmpty => Ok(actors.asJson)
+          case _ => NoContent()
+        }
+      // Add a new actor
+      case req@POST -> Root / "actors" =>
+        req.decodeJson[Actor]
+          .flatMap(moviesRepository.addActor)
+          .flatMap {
+            case true => Created("Created!")
+            case false => NotAcceptable("This actor already exists!")
+          }
+      // Update an actor
+      case req@PUT -> Root / "actors" / actor =>
+        val exist = moviesRepository.findActorByName(actor)
+        exist.flatMap {
+          case true =>
+            req.decodeJson[Actor]
+              .flatMap(newActor => moviesRepository.updateActor(actor, newActor))
+              .flatMap(_ => Ok("Updated successfully"))
+          case _ => NotFound(s"No actor found for: $actor")
+        }
+      // Delete an actor
+      case DELETE -> Root / "actors" / actor =>
+        val exist = moviesRepository.findActorByName(actor)
+        exist.flatMap {
+          case true => moviesRepository.deleteActor(actor).flatMap(_ => Ok("Deleted successfully"))
+          case _ => NotFound(s"No actor found for: $actor")
+        }
+    }
+  }
+
+  private def allRoutes[F[_] : Async](moviesRepository: MoviesRepository[F]): HttpRoutes[F] = {
+    /* Combine of semigroups */
+    movieRoutes[F](moviesRepository) <+> directorRoutes[F](moviesRepository) <+> actorRoutes[F](moviesRepository)
+  }
+
+  def allRoutesComplete[F[_] : Async](moviesRepository: MoviesRepository[F]): HttpApp[F] =
     allRoutes[F](moviesRepository).orNotFound
 }
