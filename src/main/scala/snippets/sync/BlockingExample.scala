d238e4e108fa2fd930896fe4b592fd5e7ff8985e
mattiaachilli <mattia.achilli@studio.unibo.it>
2021-12-03 16:28:06 +0100

    Fix

--- src/main/scala/snippets/sync/BlockingExample.scala
+++ src/main/scala/snippets/sync/BlockingExample.scala
@@ -7,7 +7,7 @@ import scala.io.Source
 object BlockingExample extends IOApp {
   override def run(args: List[String]): IO[ExitCode] = {
     for {
-      f <- Sync[IO].blocking(Source.fromFile("files/prova.txt"))
+      f <- Sync[IO].blocking(Source.fromFile("prova.txt"))
       s <- Sync[IO].blocking(f.mkString)
       _ <- Sync[IO].blocking(f.close()) >> IO.println(s)
     } yield ExitCode.Success
